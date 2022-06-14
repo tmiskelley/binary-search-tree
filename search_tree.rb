b61c9e92f65fa1df1c5a88ff08e3b1616304fdab
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-06-14 09:45:19 -0400

    Added root node declaration to "build_tree" method

--- search_tree.rb
+++ search_tree.rb
@@ -2,14 +2,20 @@
 
 class Tree
   def initialize(arr)
-    @root = build_tree
+    @root = build_tree(arr)
   end
 
-  def build_tree; end
+  def build_tree(arr)
+    root = (arr.length) / 2
+    Node.new(arr[root])
+  end
 end
 
 # binary tree node
 class Node
+  attr_reader :data
+  attr_accessor :left_branch, :right_branch
+
   def initialize(data)
     @data = data
     @left_branch = nil
