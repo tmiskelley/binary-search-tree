174ab2f6c9caeac7bd08a990ccb615f541679798
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-06-14 09:15:15 -0400

    Created "Tree" class

--- search_tree.rb
+++ search_tree.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+class Tree
+  def initialize(arr)
+    @root = build_tree
+  end
+
+  def build_tree; end
+end
+
 # binary tree node
 class Node
   def initialize(data)
