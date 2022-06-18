53c31d4689eee4534f325b3883c68692e27aa355
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-06-17 21:51:48 -0400

    Completed "build_tree" method

--- search_tree.rb
+++ search_tree.rb
@@ -1,20 +1,29 @@
 # frozen_string_literal: true
 
 class Tree
+  attr_accessor :root, :data
+
   def initialize(arr)
-    @root = build_tree(arr)
+    @data = arr.sort.uniq
+    @root = build_tree(data)
   end
 
-  def build_tree(arr)
-    root = (arr.length) / 2
-    Node.new(arr[root])
+  def build_tree(array)
+    return nil if array.empty?
+
+    middle = (array.size - 1) / 2
+    root_node = Node.new(array[middle])
+
+    root_node.left = build_tree(array[0...middle])
+    root_node.right = build_tree(array[(middle + 1)..-1])
+
+    root_node
   end
 end
 
 # binary tree node
 class Node
-  attr_reader :data
-  attr_accessor :left_branch, :right_branch
+  attr_accessor :left, :right, :data
 
   def initialize(data)
     @data = data
