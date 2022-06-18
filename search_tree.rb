75e1b19f5bed392382e40844f5beceb1ced9980a
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-06-17 21:53:10 -0400

    Added "pretty_print" method

--- search_tree.rb
+++ search_tree.rb
@@ -19,6 +19,12 @@ class Tree
 
     root_node
   end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
 
 # binary tree node
